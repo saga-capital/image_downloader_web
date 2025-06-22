cae75cbda568cdd119a2b61754874d104fdb2902
Hamidreza Bayat <hamidrezabayat76@gmail.com>
2025-06-22 11:31:12 +0330

    Use package:web instead of universal_html to enable wasm

--- lib/src/web_image_downloader.dart
+++ lib/src/web_image_downloader.dart
@@ -3,7 +3,7 @@ import 'dart:typed_data';
 import 'package:flutter/painting.dart';
 import 'package:http/http.dart' as http;
 import 'package:image_downloader_web/src/image_type.dart';
-import 'package:universal_html/html.dart' as html;
+import 'package:web/web.dart' as web;
 
 class WebImageDownloader {
   /// Download image from URL to user's device. It works only for Flutter web.
@@ -21,11 +21,10 @@ class WebImageDownloader {
 
     if (res.statusCode == 200) {
       await downloadImageFromUInt8List(
-        uInt8List: res.bodyBytes,
-        imageQuality: imageQuality,
-        name: name,
-        imageType: imageType
-      );
+          uInt8List: res.bodyBytes,
+          imageQuality: imageQuality,
+          name: name,
+          imageType: imageType);
     } else {
       throw Exception(res.statusCode);
     }
@@ -40,10 +39,9 @@ class WebImageDownloader {
   }) async {
     final image = await decodeImageFromList(uInt8List);
 
-    final html.CanvasElement canvas = html.CanvasElement(
-      height: image.height,
-      width: image.width,
-    );
+    final web.HTMLCanvasElement canvas = web.HTMLCanvasElement();
+    canvas.height = image.height;
+    canvas.width = image.width;
 
     final ctx = canvas.context2D;
 
@@ -55,19 +53,19 @@ class WebImageDownloader {
     }
     final data = binaryString.join();
 
-    final base64 = html.window.btoa(data);
+    final base64 = web.window.btoa(data);
 
-    final img = html.ImageElement();
+    final img = web.HTMLImageElement();
 
     img.src = "data:${imageType.format};base64,$base64";
 
-    final html.ElementStream<html.Event> loadStream = img.onLoad;
+    final web.ElementStream<web.Event> loadStream = img.onLoad;
 
     loadStream.listen((event) {
       ctx.drawImage(img, 0, 0);
       final dataUrl = canvas.toDataUrl(imageType.format, imageQuality);
-      final html.AnchorElement anchorElement =
-          html.AnchorElement(href: dataUrl);
+      final web.HTMLAnchorElement anchorElement = web.HTMLAnchorElement();
+      anchorElement.href = dataUrl;
       anchorElement.download = name ?? dataUrl;
       anchorElement.click();
     });
